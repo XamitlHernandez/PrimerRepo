d7cb86167da7907d50557a5de6a7ee14878009c2
XamitlHernandez <xamitlhernandez@gmail.com>
2024-11-13 13:12:55 -0600

    Ejercicio 2

---
+++ Ejercicio 2/index.js
@@ -0,0 +1,26 @@
+// Part 1: Verify and Run Tests
+// Download the file and navigate using the terminal inside the js folder.
+// Run the following command to install the dependencies using NPM:
+//   npm install
+// Run the unit tests and verify the results:
+//   npm test
+// Open the js folder on Visual Studio Code and verify the existing test.
+// Part 2: Implement Calculator test
+// Implement 2 unit tests for the divide function.
+// Implement 2 unit tests for the multiply function.
+// Part 3: Implement UserController test
+// Implement 1 test for the add function that verifies a user that is not in the users' list.
+// Implement 1 test for the remove function that verifies a user that is not in the users' list.
+// Implement 2 unit tests for the findByEmail function.
+// Implement 2 unit tests for the findById function.
+// Challenge Yourself
+// In Test Driven Development you write the first test that fails, and then you add the code so test can pass.
+
+// Write a unit test to verify the function divide by passing 0 as the second argument.
+// Fix the divide function so the test passes and it does handle the case for division by 0.    
+/* pruebas unitarias
+Jest -> stack tecnologías
+construir y luego probar
+probar y luego construir
+Creo una prueba: definimos datos de entrada, datos de salida, y finalmente el proceso y después codfico
+jest facilita y tiene capacidad de hacer testing automatizado - semi-automatizado     */
